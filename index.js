e278631d464b0105cde2be79d6d8e8a570bb35f5
unknown <talanvor_divine@yahoo.com>
2024-07-08 22:01:54 -0500

    Changed the way the SVG is being generated, now the text alignment is a little better since all elements are being handled by each shape class constructor. Updated the jest testing to reflect the added complexity.

--- index.js
+++ index.js
@@ -4,21 +4,19 @@ const { Shape, Triangle, Square, Circle } = require("./lib/shapes.js");
 const namedColors = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod',  'darkgray', 'darkgreen', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'greenyellow', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen',  'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'rebeccapurple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen'];
 
 const writeToFile = (text, textColor, shape, shapeColor) => { // Takes in the text for the logo, the text color, the shape and the shape color to create the SVG file
-    let newShape;
+    let newShape; // Creates the newShape object using the class constructors
     if (shape === "Triangle"){
         newShape = new Triangle();
     } else if (shape === "Square"){
         newShape = new Square();
     } else newShape = new Circle();
 
-    newShape.setBackgroundColor(shapeColor);
+    newShape.setShapeProperties(text, textColor, shapeColor);
 
     fs.writeFile('./examples/logo.svg', `
   <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
   ${newShape.render()}
-
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
 
 </svg>`,
     (err) => err ? console.error(err) : console.log("Generated logo.svg"));
@@ -74,5 +72,8 @@ const prompts = [ // Array of prompts for the user to provide information regard
 inquirer
   .prompt(prompts) // Prompting the user for information regarding the SVG file to be generated
   .then(response => { // Returns the response from the user and passes the values to be used
-    writeToFile(response.svgTextLogo, response.svgTextColor, response.svgShape, response.svgShapeColor); // Calls the function to write the SVG file   
+    writeToFile(response.svgTextLogo, response.svgTextColor, response.svgShape, response.svgShapeColor); // Calls the function to write the SVG file  
+  })
+  .catch((error) => {
+    console.error('Error within the inquirer prompt', error);
   });
